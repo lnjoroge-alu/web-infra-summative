5cf68c8275ea2d7a5b91bdbc0c1521ed5c3db156
lnjoroge-alu <l.njoroge@alustudent.com>
2026-07-30 20:39:41 +0300

    feat: add variable for jobs api response and form event listener

--- public/script.js
+++ public/script.js
@@ -2,3 +2,9 @@ const form = document.getElementById('search-form');
 const statusDiv = document.getElementById('status');
 const resultsDiv = document.getElementById('results');
 
+let currentJobs = [];
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  await searchJobs();
+});
