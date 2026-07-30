ff89a06bc9fa84d8362da7f6b52a9e579e6878d8
lnjoroge-alu <l.njoroge@alustudent.com>
2026-07-30 20:04:25 +0300

    set up basic express server

---
+++ server.js
@@ -0,0 +1,14 @@
+// Job Finder - simple Express server
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// serve the frontend
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Job Finder running on http://localhost:${PORT}`);
+});
